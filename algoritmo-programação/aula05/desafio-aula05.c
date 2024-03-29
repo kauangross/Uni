280bf592917dc585b810fa07d0e75fdd1ac70b2f
Kauan Gross <kauangross@gmail.com>
2024-03-29 19:13:03 -0300

    Update desafio-aula05.c

--- algoritmo-programação/aula05/desafio-aula05.c
+++ algoritmo-programação/aula05/desafio-aula05.c
@@ -2,45 +2,43 @@
 
 int main()
 {
-    /*
-        1 - inteiro n = numero de botas individuais
-        n linhas - = uma bota ( inteiro M e uma letra L )
-                                m = tamanho     l = lado da bota 
-
-        saida :
-        numero total de pares corretos
-    */
-
-    // Número de sapatos
-    int sapatos = 4;
-
-    // Tamanho do sapato {37, 38, 39, 40, 41} referente ao num de sapatos definidos
-    int tamanho[sapatos];
-
-    // Lado do pé {E, D} referente ao num de sapatos definidos
-    char lado[sapatos];
-    
-    // Número de pares completos
-    int parcompleto = 0;
-    
-    // For para definir o tamanho e o lado dos sapatos
-    for (int i = 0; i < sapatos; i++)
+    int sapatos;
+    while (sapatos != 0) // Repete o algoritmo enquanto o numero de sapatos não for zero
     {
-        scanf("%d %c", &tamanho[i], &lado[i]);
-    }
+        // Número de sapatos
+        printf("Numero de sapatos: ");
+        scanf("%d", &sapatos);
 
-    // For dentro de outro for para passar por todos os sapatos
-    for (int i = 0; i < sapatos; i++)
-    {
-        for (int j = (i+1); j < sapatos; j++)
+        // Tamanho do sapato {37, 38, 39, 40, 41} referente ao num de sapatos definidos
+        int tamanho[sapatos];
+        // Lado do pé {E, D} referente ao num de sapatos definidos
+        char lado[sapatos];
+        // Número de pares completos
+        int parcompleto = 0;
+        
+        // For para definir o tamanho e o lado dos sapatos
+        for (int i = 0; i < sapatos; i++)
         {
-            if ((tamanho[i] == tamanho[j]) && ( lado[i] != lado[j])) // Verificador de sapatos complementares
+            scanf("%d %c", &tamanho[i], &lado[i]);
+        }
+
+        // For dentro de outro for para passar por todos os sapatos
+        for (int i = 0; i < sapatos; i++)
+        {
+            for (int j = (i+1); j < sapatos; j++)
             {
-                parcompleto++;
+                if ((tamanho[i] == tamanho[j]) && ( lado[i] != lado[j])) // Verificador de sapatos complementares
+                {
+                    parcompleto++;
+                }
             }
         }
+        if (parcompleto == 1) // Imprime o num de pares completos
+        {
+            printf("%d par de sapatos completo\n", parcompleto); // 1 par completo
+        } else {
+            printf("%d pares de sapatos completos\n", parcompleto); // Mais de 1 par completo
+        }
     }
-    printf("%d", parcompleto);
-
     return 0;
 }
